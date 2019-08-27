8c2bf59b52382cb596851224a21bd069ab3ad0cb
baldm0mma <jev.forsberg@gmail.com>
2019-08-27 14:05:03 -0600

    Fully test actions
    Co-authored-by: Evan Markowitz <emarkowitz1@gmail.com>
    Co-authored-by: Jev Forsberg <jev.forsberg@gmail.com>

---
+++ src/utilities/mockData.js
@@ -0,0 +1,35 @@
+export const projects = [
+  {
+    id: 1,
+    project_name: 'YumTimTumTam'
+  },
+  {
+    id: 2,
+    project_name: 'SkippityTippity'
+  }
+];
+
+export const palettes = [
+  {
+    id: 1,
+    project_id: 1,
+    palette_name: 'TimTamSlammy',
+    color_1: 'aaaaaa',
+    color_2: 'bbbbbb',
+    color_3: 'cccccc',
+    color_4: 'dddddd',
+    color_5: 'eeeeee'
+  },
+  {
+    id: 2,
+    project_id: 1,
+    palette_name: 'BiffBoffDiffDoff',
+    color_1: '111111',
+    color_2: '222222',
+    color_3: '333333',
+    color_4: '444444',
+    color_5: '555555'
+  }
+];
+
+export const errorMessage = "Nah man, that ain't gonna work for me";
